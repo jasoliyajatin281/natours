ef29e0f7fc5b36760074888e6e4c56d55a05f87d
Jatin Jasoliya <jasoliyajatin281@gmail.com>
2020-09-27 19:08:42 -0400

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,17 +9,18 @@ const stripe = Stripe(
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/booking/checkout-session/${tourId}`
-    );
-    console.log(session);
+
+    // Use this url on development
+    // `http://127.0.0.1:3000/api/v1/booking/checkout-session/${tourId}`
+    const session = await axios(`/api/v1/booking/checkout-session/${tourId}`);
+    // console.log(session);
 
     // 2) Create checkout form  + change credit card
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id,
     });
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     showAlert("error", err);
   }
 };
