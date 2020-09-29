8a5b4c971f011c1c0ca2e7e76ea27b745a70aa0b
Jatin Jasoliya <jasoliyajatin281@gmail.com>
2020-09-29 12:24:59 -0400

    Improved stripe implementation 5

--- controllers/bookinController.js
+++ controllers/bookinController.js
@@ -1,11 +1,3 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const Tour = require("../models/tourModel");
-const Booking = require("../models/bookingModel");
-const catchAsync = require("../utils/catchAsync");
-const factory = require("./handlerFactory");
-const AppError = require("../utils/appError");
-const User = require("../models/userModel");
-
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
@@ -68,9 +60,3 @@ exports.webhookCheckout = (req, res, next) => {
 
   res.status(200).json({ received: true });
 };
-
-exports.createBooking = factory.createOne(Booking);
-exports.getBooking = factory.getOne(Booking);
-exports.getAllBooking = factory.getAll(Booking);
-exports.updateBooking = factory.updateOne(Booking);
-exports.deleteBooking = factory.deleteOne(Booking);
--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const bookingController = require("./../controllers/bookinController");
+const bookingController = require("./../controllers/bookingController");
 const authController = require("./../controllers/authController");
 
 const router = express.Router();
+
 router.use(authController.protect);
 
 router.get("/checkout-session/:tourId", bookingController.getCheckoutSession);
@@ -11,7 +12,7 @@ router.use(authController.restrictTo("admin", "lead-guide"));
 
 router
   .route("/")
-  .get(bookingController.getAllBooking)
+  .get(bookingController.getAllBookings)
   .post(bookingController.createBooking);
 
 router
