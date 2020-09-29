e75e1eb8aa96f065e9f9fffa9316253a491fe973
Jatin Jasoliya <jasoliyajatin281@gmail.com>
2020-09-29 11:28:12 -0400

    Improved striped implementation3

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -9,18 +9,15 @@ const stripe = Stripe(
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
-
-    // Use this url on development
-    // `http://127.0.0.1:3000/api/v1/booking/checkout-session/${tourId}`
-    const session = await axios(`/api/v1/booking/checkout-session/${tourId}`);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     // console.log(session);
 
-    // 2) Create checkout form  + change credit card
+    // 2) Create checkout form + chanre credit card
     await stripe.redirectToCheckout({
       sessionId: session.data.session.id,
     });
   } catch (err) {
-    // console.log(err);
+    console.log(err);
     showAlert("error", err);
   }
 };
