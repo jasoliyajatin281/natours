54f3a606b5f82672b920523e128fb15345e94d84
Jatin Jasoliya <jasoliyajatin281@gmail.com>
2020-09-29 12:16:55 -0400

    Improved stripe implementation 4

--- controllers/bookinController.js
+++ controllers/bookinController.js
@@ -7,17 +7,17 @@ const AppError = require("../utils/appError");
 const User = require("../models/userModel");
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  // 1) Get the currently get booked tour
+  // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
+  // console.log(tour);
 
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
-
-    // success_url: `${req.protocol}://${req.get("host")}/my-tours/?tour=${
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get("host")}/my-tours`,
+    success_url: `${req.protocol}://${req.get("host")}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -29,28 +29,18 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           `${req.protocol}://${req.get("host")}/img/tours/${tour.imageCover}`,
         ],
         amount: tour.price * 100,
-        currency: "CAD",
+        currency: "usd",
         quantity: 1,
       },
     ],
   });
 
-  // 3) Create session as response (Send to thge client)
+  // 3) Create session as response
   res.status(200).json({
-    status: "Success",
+    status: "success",
     session,
   });
 });
-
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
-//   const { tour, user, price } = req.query;
-
-//   if (!tour && !user && !price) return next();
-//   await Booking.create({ tour, user, price });
-
-//   res.redirect(req.originalUrl.split("?")[0]);
-// });
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -5,13 +5,10 @@ const authController = require("./../controllers/authController");
 const router = express.Router();
 router.use(authController.protect);
 
-router.get(
-  "/checkout-session/:tourId",
-  authController.protect,
-  bookingController.getCheckoutSession
-);
+router.get("/checkout-session/:tourId", bookingController.getCheckoutSession);
 
 router.use(authController.restrictTo("admin", "lead-guide"));
+
 router
   .route("/")
   .get(bookingController.getAllBooking)
